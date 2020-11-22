4d4fd5ce201304cd4adb02ae9a6f219b8151f39d
Alessandro Contenti <a.contenti@gmail.com>
2020-11-22 18:31:06 +0100

    bug fixes
    Signed-off-by: Alessandro Contenti <a.contenti@gmail.com>

--- quasar.conf.js
+++ quasar.conf.js
@@ -45,7 +45,7 @@ module.exports = configure(function (/* ctx */) {
 
 		// Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
 		build: {
-			vueRouterMode: "hash", // available values: 'hash', 'history'
+			vueRouterMode: "history", // available values: 'hash', 'history'
 
 			// transpile: false,
 
@@ -78,7 +78,7 @@ module.exports = configure(function (/* ctx */) {
 		// https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
 		framework: {
 			iconSet: "material-icons", // Quasar icon set
-			lang: "it-it", // Quasar language pack
+			lang: "it", // Quasar language pack
 			config: {},
 
 			// Possible values for "importStrategy":
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -5,7 +5,14 @@ const routes: RouteConfig[] = [
 		path: "/",
 		component: () => import("layouts/MainLayout.vue"),
 		children: [
-			{path: ":place", component: () => import("pages/Place.vue")}
+			{
+				path: ":place",
+				component: () => import("pages/Place.vue")
+			},
+			{
+				path: "",
+				redirect: "italia"
+			}
 		]
 	},
 	{
